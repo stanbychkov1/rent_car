ccd23516645f04bcdeb45b789a085b9297506985
S B <stanbychkov1@yandex.ru>
2022-08-30 23:12:13 +0300

    added date validation in case of direct requests

--- cars/forms.py
+++ cars/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 
 from cars import models
 
@@ -13,3 +14,14 @@ class RentalPeriodForm(forms.ModelForm):
     class Meta:
         model = models.RentalPeriod
         exclude = ('driver',)
+
+    def is_valid(self):
+        excluded_cars = models.RentalPeriod.objects.filter(
+            Q(start_date__range=(self.instance['start_date'],
+                                 self.instance['end_date'])) |
+            Q(end_date__range=(self.instance['start_date'],
+                               self.instance['end_date']))
+        ).values_list('car_id')
+        if self.instance['car'] in excluded_cars:
+            return False
+        return super().is_valid()
