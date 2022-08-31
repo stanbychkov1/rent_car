d1844b71edeee4aae08f39d7187aa4f5e35b62f2
S B <stanbychkov1@yandex.ru>
2022-08-31 18:26:10 +0300

    tests for application done

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,12 @@
 import pytest
+import datetime
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 from cars import models
+
+from users import forms
+
 
 User = get_user_model()
 
@@ -45,3 +49,119 @@ class TestAPP:
 
         assert response.status_code in (301, 302), \
             f'Авторизация пользователя недоступна'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_03_index_not_auth(self, client):
+        url = '/'
+        response = client.get(url)
+
+        assert response.status_code == 302, \
+            f'Проверьте, что неавторизованный пользователь не может получить' \
+            f'доступ к {url}'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_04_index(self, user_client):
+        url = '/'
+
+        response = user_client.get(url)
+
+        assert response.status_code == 200, \
+            f'Проверьте, что авторизованный пользователь может получить' \
+            f'доступ к {url}'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_05_rent_not_auth(self, client):
+        url = '/rent/'
+        response = client.get(url)
+
+        assert response.status_code == 302, \
+            f'Проверьте, что неавторизованный пользователь не может получить' \
+            f'доступ к {url}'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_06_rent(self, user_client, user, admin):
+        start_date = datetime.date.today()
+        end_date = datetime.date.today()+datetime.timedelta(days=1)
+        url = f'/rent/?start_date={start_date}' \
+              f'&end_date={end_date}'
+        cars = models.Car.objects.bulk_create([
+            models.Car(title='BMW', prod_year='1990', owner_id=user.id),
+            models.Car(title='Mercedes', prod_year='1990', owner_id=admin.id)
+        ])
+        response = user_client.get(url)
+
+        assert response.status_code == 200, \
+            f'Проверьте, что можно получить доступ к автомобилям'
+
+        assert len(response.context_data['object_list']) == 2, \
+            f'Проверьте, что сервис выдает правильные данные'
+
+        models.RentalPeriod.objects.create(
+            car_id=cars[0].id, driver_id=user.id,
+            start_date=start_date, end_date=end_date
+        )
+
+        response = user_client.get(url)
+
+        assert len(response.context_data['object_list']) == 1, \
+            f'Проверьте, что сервис выдает правильные данные'
+
+        data = {
+            'car': 2,
+            'driver': admin.id,
+            'start_date': start_date.__str__(),
+            'end_date': end_date.__str__()
+        }
+        new_response = user_client.post(url, data=data)
+
+        assert new_response.status_code in (301, 302), \
+            f'Проверьте, что можно арендовать автомобиль'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_07_profile_not_auth(self, client):
+        url = '/auth/profile/'
+        response = client.get(url)
+
+        assert response.status_code == 302, \
+            f'Проверьте, что неавторизованный пользователь не может получить' \
+            f'доступ к {url}'
+
+        url = '/auth/profile/update/'
+        response = client.get(url)
+
+        assert response.status_code == 302, \
+            f'Проверьте, что неавторизованный пользователь не может получить' \
+            f'доступ к {url}'
+
+    @pytest.mark.django_db(transaction=True)
+    def test_08_profile(self, user_client, user):
+        url = '/auth/profile/'
+        response = user_client.get(url)
+
+        assert response.status_code == 200, \
+            f'Проверьте, что можно получить доступ к данным профиля'
+        assert response.context_data['form'].__class__ is forms.UserForm, \
+            f'Проверьте, что отображаются правильные данные'
+
+        url = '/auth/profile/update/?'
+        response = user_client.get(url)
+
+        assert response.status_code == 200, \
+            f'Проверьте, что можно получить доступ к данным профиля'
+
+        data = {
+            'first_name': 'stanley',
+            'last_name': 'stanley',
+            'email': user.email,
+            'language': 'EN',
+            'username': 'stanley'
+        }
+        response = user_client.post(url, data=data)
+
+        assert response.status_code in (301, 302), \
+            f'Проверьте, что сервис дает изменить данные пользователя'
+        user.refresh_from_db()
+        assert user.first_name == 'stanley', \
+            f'Проверьте, что сервис дает изменить данные пользователя'
+
+
